9e344b6a53a0fcc31f395612f883840011bb4763
Yogesh Darji <yogesh_darji99@yahoo.com>
2016-01-08 13:44:19 -0600

    Update MainActivity.java

--- breakoutgame/app/src/main/java/com/example/yogeshdarji/breakout/MainActivity.java
+++ breakoutgame/app/src/main/java/com/example/yogeshdarji/breakout/MainActivity.java
@@ -67,6 +67,7 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+        //to the item id
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
